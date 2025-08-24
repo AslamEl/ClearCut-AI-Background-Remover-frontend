8c7762efbeaf3192865fe110e9e61bdbd6e38097
aslam_mhm <mhdroman171@gmail.com>
2025-08-24 13:57:08 +0530

    database

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import Footer from "./components/Footer.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Route, Routes } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
+import UserSyncHandler from "./components/UserSyncHandler.jsx";
 
 
 const App =()=> {
     return(
         <div>
+            <UserSyncHandler/>
             <Menubar/>
             <Toaster/>
             <Routes>
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { ClerkProvider } from '@clerk/clerk-react'
+import  {AppContextProvider} from './context/AppContext.jsx'
 
 
 const PUBLISHABLEKEY= import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
@@ -14,7 +15,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   
     <BrowserRouter>
       <ClerkProvider publishableKey={PUBLISHABLEKEY}>
-          <App/>
+          <AppContextProvider>
+            <App/>
+          </AppContextProvider>
       </ClerkProvider>
     </BrowserRouter>
   
